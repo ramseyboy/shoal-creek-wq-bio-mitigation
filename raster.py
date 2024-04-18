56163f8bb6d2db08f8be3fa02b1432ad2b958426
Walker Hannan <awahannan@gmail.com>
2024-04-17 19:30:58 -0500

    Add impervious cover, land use and land use change to project

--- raster.py
+++ raster.py
@@ -1,5 +1,6 @@
 from os import path
 
+import numpy as np
 import rasterio
 import rasterio.mask
 import shapely
@@ -17,3 +18,19 @@ def clip_raster(src_file_path: str, out_file_path: str, clip_geom: shapely.Geome
 
     with rasterio.open(out_file_path, "w", **out_meta) as dest:
         dest.write(out_image)
+
+
+def fix_no_data_value(src_file_path: str, out_file_path: str, no_data_value=0):
+    with rasterio.open(src_file_path, "r+") as src:
+        src.nodata = no_data_value
+        with rasterio.open(out_file_path, 'w', **src.profile) as dst:
+            for i in range(1, src.count + 1):
+                band = src.read(i)
+                band = np.where(band == no_data_value, no_data_value, band)
+                dst.write(band, i)
+
+
+if __name__ == '__main__':
+    fix_no_data_value("data/shoal_creek_nlcd_land_cover_change.tif", "data/shoal_creek_nlcd_land_cover_change1.tif", no_data_value=0)
+    fix_no_data_value("data/shoal_creek_nlcd_imperviousness.tif", "data/shoal_creek_nlcd_imperviousness1.tif", no_data_value=0)
+    fix_no_data_value("data/shoal_creek_nlcd_land_cover.tif", "data/shoal_creek_nlcd_land_cover1.tif", no_data_value=0)
